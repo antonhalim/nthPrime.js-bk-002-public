6066d0c1eb50ce956800456f280d62a8e10e5fce
Blake Johnson <blake41@gmail.com>
2015-05-01 17:33:23 -0400

    non oo

--- spec/prime.spec.js
+++ spec/prime.spec.js
@@ -3,24 +3,24 @@
 describe('Prime', function() {
 
   it('first', function(){
-    expect(prime.nth(1)).toEqual(2);
+    expect(nth(1)).toEqual(2);
   });
 
   it('second', function(){
-    expect(prime.nth(2)).toEqual(3);
+    expect(nth(2)).toEqual(3);
   });
 
   it('sixth', function(){
-    expect(prime.nth(6)).toEqual(13);
+    expect(nth(6)).toEqual(13);
   });
 
   it('big prime', function(){
-    expect(prime.nth(10001)).toEqual(104743);
+    expect(nth(10001)).toEqual(104743);
   });
 
   it('weird case',function() {
     expect(function () {
-      prime.nth(0);
+      nth(0);
     }).toThrow(new Error("Prime is not possible"));
   });
 
